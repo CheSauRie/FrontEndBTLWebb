b12ef755ea32871dbad8c85330070fd51e19eb29
cheSau <hongquanvd1@gmail.com>
2022-12-23 22:31:34 +0700

    Admin screen

---
+++ login/login.js
@@ -0,0 +1,28 @@
+
+let role;
+const form = document.getElementById("form-login")
+form.addEventListener('submit', (e) => {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    e.preventDefault();
+    axios
+        .post("http://localhost:3001/api/v1/users/login", {
+            email,
+            password
+        })
+        .then(function (response) {
+            role = response.data.type
+            if (role == "Ban điều hành") {
+                window.location.assign("../admin/admin.html");
+                alert("Chào ban quản lý")
+            }
+            console.log(response.data);
+            console.log(role);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+})
+
+
